33631d3187e0f4f8e658b1e20463067ea579e667
Joshua Forster <joshuaforster@Joshuas-MacBook-Air-3.local>
2026-03-05 20:55:25 +0000

    fixed docker

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,9 +3,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT ?? 3000;
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
